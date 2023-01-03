438b276ef243ffd83cf3997ead1d3adf422a6333
Gal Lindič <2stskier@gmail.com>
2023-01-03 22:06:14 +0100

    Fix

--- lib/src/main/java/si/fri/rso/deliverymicroservice/lib/DeliveryStatusDto.java
+++ lib/src/main/java/si/fri/rso/deliverymicroservice/lib/DeliveryStatusDto.java
@@ -7,6 +7,10 @@ public class DeliveryStatusDto {
         this.delivered = status;
     }
 
+    public DeliveryStatusDto() {
+
+    }
+
     public Boolean getDelivered() {
         return delivered;
     }
